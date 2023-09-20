54eab98b43c5ae049f089d80ce9dbdf6d6a985ae
Samuel Barnes <samuelcbarnes@gmail.com>
2023-09-20 16:26:57 -0400

    add publicPath for vue configuration

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
 })
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/tnpremierproperties/" : "/",
   css: {
     loaderOptions: {
       sass: {
